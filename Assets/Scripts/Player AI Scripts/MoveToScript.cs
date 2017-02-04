2bb4b2ad49919d9cc7f74067c3459afe95489863
Zach Shuffield <zshuff03@yahoo.com>
2017-02-04 16:05:01 -0500

    Imported some basic store assets/added movement

---
+++ Assets/Scripts/Player AI Scripts/MoveToScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToScript : MonoBehaviour {
+  public UnityEngine.AI.NavMeshAgent agent;
+  // Use this for initialization;
+	void Start () {
+	}
+
+  public void setDestination (GameObject target) {
+    agent.destination = target.transform.position;
+  }
+}
